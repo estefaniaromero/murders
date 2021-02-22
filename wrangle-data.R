279013c1779bf580c6de84f02583e8f31ff68858
Karla Estefania Romero <70114224+estefaniaromero@users.noreply.github.com>
2021-02-21 21:30:02 -0800

    Create wrangle data

--- wrangle-data.R
+++ wrangle-data.R
@@ -1,4 +1,4 @@
-#Now we are ready to write a script to read this data and prepare a table that we can use for analysis. Call the file wrangle-data.R
+# Call the file wrangle-data.R
 library(tidyverse)
 murders <- read_csv("data/murders.csv")
 murders <-murders %>% mutate(region = factor(region),
